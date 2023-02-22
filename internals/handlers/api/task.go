4558a81d6800b73c6aa85f80befa307d065a2197
aleshkin907 <aleshkin.907@gmail.com>
2023-02-22 23:38:39 +0600

    add some api)))

--- internals/handlers/api/task.go
+++ internals/handlers/api/task.go
@@ -4,6 +4,22 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+func GetTasks(c *fiber.Ctx) error {
+	return c.JSON(fiber.Map{"status": "debug", "message": "GetTasksHandler", "data": nil})
+}
+
 func GetTask(c *fiber.Ctx) error {
-	return c.JSON(fiber.Map{"status": "debug", "message": "[API] GetTask", "data": nil})
+	return c.JSON(fiber.Map{"status": "debug", "message": "GetTaskIDHandler", "data": nil})
+}
+
+func RemoveTask(c *fiber.Ctx) error {
+	return c.JSON(fiber.Map{"status": "debug", "message": "RemoveTaskHandler", "data": nil})
+}
+
+func UpdateTask(c *fiber.Ctx) error {
+	return c.JSON(fiber.Map{"status": "debug", "message": "UpdateTaskHandler", "data": nil})
+}
+
+func CreateTask(c *fiber.Ctx) error {
+	return c.JSON(fiber.Map{"status": "debug", "message": "CreateTaskHandler", "data": nil})
 }
